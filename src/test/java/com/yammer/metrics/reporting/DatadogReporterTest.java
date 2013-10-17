fdfcc77cff795ffa95a00a1b974358fdf050b576
Daniel Chia <danchia@coursera.org>
2013-10-17 13:45:20 -0700

    tests for metrics expansion

--- src/test/java/com/yammer/metrics/reporting/DatadogReporterTest.java
+++ src/test/java/com/yammer/metrics/reporting/DatadogReporterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +23,8 @@ import com.yammer.metrics.core.Meter;
 import com.yammer.metrics.core.MetricPredicate;
 import com.yammer.metrics.core.MetricsRegistry;
 import com.yammer.metrics.core.VirtualMachineMetrics;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.reporting.DatadogReporter.Expansions;
 
 public class DatadogReporterTest {
 
@@ -96,6 +99,44 @@ public class DatadogReporterTest {
     assertEquals("gauge", gaugeEntry.get("type"));
     points = (List<List<Number>>) gaugeEntry.get("points");
     assertEquals(123, points.get(0).get(1));
+  }
+
+  @Test
+  public void testTimerExpansion() throws JsonParseException, JsonMappingException,
+      IOException {
+    // TODO: punting on testing actual values, as Timer is a class not an interface,
+    // and requires a threadPool and all that.
+    Timer timer = metricsRegistry.newTimer(DatadogReporterTest.class, "my.timer");
+    timer.update(1, TimeUnit.MILLISECONDS);
+
+    for (Expansions expansion: Expansions.ALL) {
+      expansionTestHelper(expansion);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void expansionTestHelper(Expansions expansion) throws JsonParseException,
+      JsonMappingException, IOException {
+    MockTransport transport = new MockTransport();
+    DatadogReporter dd = new DatadogReporter(metricsRegistry, MetricPredicate.ALL,
+      VirtualMachineMetrics.getInstance(), transport, Clock.defaultClock(),
+      "hostname", EnumSet.of(expansion), false);
+
+    assertEquals(0, transport.numRequests);
+    dd.run();
+    assertEquals(1, transport.numRequests);
+
+    String body = new String(transport.lastRequest.getPostBody(), "UTF-8");
+    Map<String, Object> request = new ObjectMapper().readValue(body,
+        HashMap.class);
+    List<Object> series = (List<Object>) request.get("series");
+
+    assertEquals(1, series.size());
+    Map<String, Object> metric = (Map<String, Object>) series.get(0);
+
+    assertEquals("com.yammer.metrics.reporting.DatadogReporterTest.my.timer." + expansion.toString(),
+      metric.get("metric"));
+    assertEquals(expansion.equals(Expansions.COUNT) ? "counter" : "gauge", metric.get("type"));
   }
 
   @Test
