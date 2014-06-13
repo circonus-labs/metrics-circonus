6716c84ac28ea1878fb4eff6b4d2d8d96e735f85
Ankur Chauhan <achauhan@brightcove.com>
2014-06-13 11:00:50 -0700

    Remove unused logger

--- src/main/java/com/yammer/metrics/reporting/Transport.java
+++ src/main/java/com/yammer/metrics/reporting/Transport.java
@@ -1,14 +1,9 @@
 package com.yammer.metrics.reporting;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.io.OutputStream;
 
 public interface Transport {
-
-  static final Logger LOG = LoggerFactory.getLogger(Transport.class);
 
   public Request prepare() throws IOException;
 
